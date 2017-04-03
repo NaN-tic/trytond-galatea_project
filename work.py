74d27d8c16caf4093a9109ca249b61a19f9a719e
Raimon Esteve <raimon@nan-tic.com>
2017-04-03 14:37:39 +0200

    Allow to show at Galatea

--- work.py
+++ work.py
@@ -9,12 +9,18 @@ __all__ = ['Work']
 class Work:
     __metaclass__ = PoolMeta
     __name__ = 'project.work'
+    galatea = fields.Boolean('Galatea',
+        help='Allow to show at Galatea')
 
     @classmethod
     def __setup__(cls):
         super(Work, cls).__setup__()
         cls.party.states['invisible'] = []
         cls.party_address.states['invisible'] = []
+
+    @staticmethod
+    def default_galatea():
+        return True
 
     @fields.depends('parent')
     def on_change_with_party(self):
